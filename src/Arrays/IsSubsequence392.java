b3e03c9f54211585a90d1bcc55e8c4373ba56660
Rahul70307 <rahul703nitp@gmail.com>
2022-09-07 00:03:00 +0530

    IsSubsequence392

---
+++ src/Arrays/IsSubsequence392.java
@@ -0,0 +1,21 @@
+
+package Arrays;
+public class IsSubsequence392 {
+
+    public boolean isSubsequence(String s, String t) {
+
+        if(s.length() == 0){
+            return true;
+        }
+        int i =0;
+        int j =0;
+
+        while(j< t.length() && i< s.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+}
